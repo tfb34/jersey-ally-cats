c1148cb168fcfd17edd82614dc31903db8992950
tfb34 <britothalia212@gmail.com>
2026-08-27 09:56:10 -0400

    Build working template layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    sanity({
+      projectId: "q9w9iv8a",
+      dataset: "production",
+      useCdn: false, // for static builds
+    }),
+  ],
+});
