d15bcb7bc7b9e8d8b32b40eaba72ef7058dc1f71
tfb34 <britothalia212@gmail.com>
2026-09-10 16:23:09 -0400

    Add cloudflare adapter to astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from '@astrojs/cloudflare';
 
 import sanity from "@sanity/astro";
 
@@ -9,7 +10,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+adapter: cloudflare(),
   integrations: [
     sanity({
       projectId: "q9w9iv8a",
